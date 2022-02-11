7ec14d872f0f561afc73e4157369be1983d90957
lelasha <lukalelashvili@gmail.com>
2022-02-12 00:54:27 +0400

    remove unnecessary code

--- src/test/java/com/emulator/bankservice/Exception/NoSuchAuthMethodException.java
+++
@@ -1,7 +0,0 @@
-package com.emulator.bankservice.Exception;
-
-public class NoSuchAuthMethodException extends RuntimeException {
-    public NoSuchAuthMethodException(String message) {
-        super(message);
-    }
-}
